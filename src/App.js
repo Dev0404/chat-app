100673f94c107cc1a282992a790217eab603c878
dev <dev.mehta9090@gmail.com>
2021-08-12 01:46:34 +0530

    added sign in page and signed in with facebook/google

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-// eslint-disable-next-line import/no-unresolved
-import 'rsuite/dist/styles/rsuite-default.css';
+import { Switch } from 'react-router';
 
+import PrivateRoute from './components/PrivateRoute';
+import home from './pages/home';
+import SignIn from './pages/SignIn';
+import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyA9RnpLdp7-4r2ylR1jkNVuyFOpo3AlPLo',
@@ -10,3 +12,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
