c4cc9c3a5d053fc013f118b4abae3adcb5963c74
dev <dev.mehta9090@gmail.com>
2021-08-19 01:23:27 +0530

    created dashboard,linked facebook and google to one account, created provider block

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
+import EditableInput from '../EditableInput';
+import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+  const onSave = async newData => {
+    const userNicknameref = database
+      .ref(`/profiles/${profile.uid}`)
+      .child('name');
+    try {
+      await userNicknameref.set(newData);
+      Alert.success('Nickname has been updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
   return (
     <>
       <Drawer.Header>
@@ -11,6 +25,14 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Heyy {profile.name}</h3>
+        <ProviderBlock />
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
